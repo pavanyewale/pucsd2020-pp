8692eff35475094524777a1f92dd93cfae4e778c
Pavan Yewale <pavan.yewale@agrostar.in>
2020-05-06 10:15:21 +0530

    basic schema added

---
+++ user_service/README.md
@@ -0,0 +1,22 @@
+# Simple HTTP Web - Server
+
+## Cloning repository
+
+/src/github.com/  
+
+
+## HOW TO COMPILE
+
+go to  /src/github.com/go-chi-mysql-service  
+
+go build
+
+## HOW TO RUN
+./main
+
+## SENDING HTTP REQUEST
+Install curl if not already installed.  
+
+command : apt install curl
+curl http://localhost:8080/Golang
+Hi there, I love Golang!
---
+++ user_service/resource/sql/001_create_table.sql
@@ -0,0 +1,50 @@
+USE restapi;
+
+CREATE TABLE IF NOT EXISTS user_detail (
+    id                  INT         AUTO_INCREMENT      PRIMARY KEY,
+    first_name          CHAR(25)    NOT NULL,
+    last_name           CHAR(25)    NOT NULL,
+    email               CHAR(64)    NOT NULL UNIQUE,
+    password            VARBINARY(128)    NOT NULL,
+    contact_number      CHAR(15)    NOT NULL,
+    updated_by          INT         NOT NULL DEFAULT 0,
+    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
+    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
+    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS user_groups (
+    id                  INT         AUTO_INCREMENT      PRIMARY KEY,
+    group_name          CHAR(25)    NOT NULL,
+    updated_by          INT         NOT NULL DEFAULT 0,
+    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
+    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
+    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS user_permission (
+    id                  INT         AUTO_INCREMENT      PRIMARY KEY,
+    permission_name          CHAR(25)    NOT NULL,
+    updated_by          INT         NOT NULL DEFAULT 0,
+    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
+    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
+    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS group_permissions (
+    group_id                  INT,
+    permission_id             INT,
+    updated_by          INT         NOT NULL DEFAULT 0,
+    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
+    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
+    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS user_permissions (
+    user_id                  INT,         
+    permission_id                  INT,         
+    updated_by          INT         NOT NULL DEFAULT 0,
+    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
+    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
+    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB CHARACTER SET=utf8;
